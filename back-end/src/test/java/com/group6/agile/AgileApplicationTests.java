4fbcbf419a428ea8e4b3b0f261d8607f53b1f6ee
canhtv05 <canhtvph51036@gmail.com>
2025-03-31 20:58:07 +0700

    init project

---
+++ back-end/src/test/java/com/group6/agile/AgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.agile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ front-end/src/components/AppFooter.vue
@@ -0,0 +1,10 @@
+<template>
+  <CFooter class="px-4">
+    <div>
+      <span>SD19303_Agile_Group6</span>
+    </div>
+    <div class="ms-auto">
+      <span>Copyright CoreUI and rebuilt by Canhtv05 x Khanh</span>
+    </div>
+  </CFooter>
+</template>
