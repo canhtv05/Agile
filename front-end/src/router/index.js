8c4359f50f8136d71ae0bebcd8d3c59751140831
canhtv05 <canhtvph51036@gmail.com>
2025-04-01 23:26:07 +0700

    T10Phát triển frontend hiển thị chi tiết sản phẩm

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -10,6 +10,11 @@ const routes = [
         component: () => import('@/views/pages/ProductDetail.vue'),
     },
     {
+        path: '/product-detail2', // Thêm :id để hỗ trợ dynamic param
+        name: 'ProductDetail2',
+        component: () => import('@/views/pages/ProductDetail2.vue'),
+    },
+    {
         path: '/product-categories', // Thêm :id để hỗ trợ dynamic param
         name: 'ProductCategories',
         component: () => import('@/views/pages/ProductCategories.vue'),
